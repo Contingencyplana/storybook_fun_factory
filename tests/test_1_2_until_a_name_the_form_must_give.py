d132c356fd9d69340048684805894da38fc6c671
Mark Roy Gillam <hyper5ai@yahoo.com.au>
2025-04-15 03:31:40 +1000

    Modified test _1_2.

--- tests/test_1_2_until_a_name_the_form_must_give.py
+++ tests/test_1_2_until_a_name_the_form_must_give.py
@@ -17,5 +17,5 @@ def test_poetic_line_to_filename():
     assert _1_2_until_a_name_the_form_must_give.poetic_line_to_filename("a name that sings yet fits the tape") == "a_name_that_sings_yet_fits_the_tape.py"
     assert _1_2_until_a_name_the_form_must_give.poetic_line_to_filename("  no_slashes break no:colons bind ") == "no_slashes_break_nocolons_bind.py"
     assert _1_2_until_a_name_the_form_must_give.poetic_line_to_filename("A Line With CAPS and Spaces") == "a_line_with_caps_and_spaces.py"
-    assert _1_2_until_a_name_the_form_must_give.poetic_line_to_filename("must-keep?it*safe|&clean!") == "must_keep_it_safe_clean.py"
+    assert _1_2_until_a_name_the_form_must_give.poetic_line_to_filename("must-keep?it*safe|&clean!") == "mustkeepitsafeclean.py"
     assert _1_2_until_a_name_the_form_must_give.poetic_line_to_filename("") == "unnamed_line.py"
